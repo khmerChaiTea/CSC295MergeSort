b550b05fa2d2816be6db8fcb6315871993058206
pchai <prempreychhay@gmail.com>
2024-07-18 15:57:24 -0700

    Add project files.

---
+++ CSC295MergeSort/Program.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace SortingAlgorithms
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //// CTRL + K + C = shortcut for comment
+            //int[] arr1 = { 90, 3, 2, 56, 32, 34, 65, 68, 76, 1, 0, 100, 8 };
+            //int[] arr2 = { 90, 3, 2, 56, 32, 34, 65, 68, 76, 1, 0, 100, 8 };
+            //int[] arr3 = { 90, 3, 2, 56, 32, 34, 65, 68, 76, 1, 0, 100, 8 };
+            //int[] arrSorted1 = { 0, 1, 2, 3, 8, 32, 34, 56, 65, 68, 76, 90, 100 };
+            //int[] arrSorted2 = { 0, 1, 2, 3, 8, 32, 34, 56, 65, 68, 76, 90, 100 };
+            //int[] arrSorted3 = { 0, 1, 2, 3, 8, 32, 34, 56, 65, 68, 76, 90, 100 };
+
+            //Stopwatch stopwatch = new Stopwatch();
+            //stopwatch.Start();
+            //InsertionSort(arr1);
+            //stopwatch.Stop();
+            //Console.WriteLine($"Elapsed time for Insertion Sort: {stopwatch.ElapsedTicks}");
+
+            //stopwatch.Restart();
+            //stopwatch.Start();
+            //BubbleSort(arr2);
+            //stopwatch.Stop();
+            //Console.WriteLine($"Elapsed time for Bubble Sort: {stopwatch.ElapsedTicks}");
+
+            //stopwatch.Restart();
+            //stopwatch.Start();
+            //SelectionSort(arr3);
+            //stopwatch.Stop();
+            //Console.WriteLine($"Elapsed time for Selection Sort: {stopwatch.ElapsedTicks}");
+
+            int[] mergeArray = { 3, 2, 5, 6, 7, 4, 1, 0 };
+            MergeSort(mergeArray);
+        }
+
+        public static void PrintArray(int[] arr)
+        {
+            foreach (int element in arr)
+            {
+                Console.Write($"{element} ");
+            }
+            Console.WriteLine();
+        }
+
+        // Responsible for splitting the array up and then merged them together
+        // Called itself recursively
+        public static void MergeSort(int[] arr)
+        {
+            if (arr.Length <= 1) return;    // example of early return
+
+            int mid = arr.Length / 2;
+            int[] leftSubArray = new int[mid];
+            int[] rightSubArray = new int[arr.Length - mid];
+
+            for (int i = 0; i < mid; i++)
+            {
+                leftSubArray[i] = arr[i];
+            }
+
+            for (int i = mid; i < arr.Length; i++)
+            {
+                rightSubArray[i - mid] = arr[i];
+            }
+
+            MergeSort(leftSubArray);
+            MergeSort(rightSubArray);
+        }
+
+        public static void InsertionSort(int[] arr)
+        {
+            // A for loop to iterate the array to sort the elements
+            // Overall worst case scenario O(n ^ 2)
+            // Best case scenario O(n)
+            for (int i = 1; i < arr.Length; i++)    // O(n) <--- O(n - 1)
+            {
+                // Temporary store an elements in the 'temp' variable
+                int temp = arr[i];
+
+                // Evaluate the elements to the left of the array with 'temp' element
+                // In this case we start at index 1 and the array started with index 0
+                int priorIndex = i - 1;
+
+                // If out prior element is greater than our stored ('temp') element
+                // and we have not reached the end of the array
+                while (priorIndex >= 0 && arr[priorIndex] > temp)   // O(n)
+                {
+                    arr[priorIndex + 1] = arr[priorIndex];
+                    priorIndex--;
+                }
+
+                arr[priorIndex + 1] = temp;
+
+            }
+            Console.WriteLine();
+        }
+
+        public static int BubbleSort(int[] arr)
+        {
+            int totalOuterIterations = 0;
+            int temp;
+            // Overall O(n^2) runtime - Big O
+            // Big Omega - O(n^2)
+            for (int i = 0; i < arr.Length - 1; i++) // O(n) how many times we need to go though the unsorted array
+            {
+                totalOuterIterations++;
+                int swaps = 0;
+                for (int j = 0; j < arr.Length - 1 - i; j++)  // O(n)
+                {
+                    // we need to swap
+                    if (arr[j] > arr[j + 1])           // can modify if '>' change to '<'
+                    {
+                        swaps++;
+                        temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+
+                if (swaps == 0) break; // break out of the for loop
+            }
+
+            return totalOuterIterations;
+        }
+
+        public static void SelectionSort(int[] arr)
+        {
+            // minIndex keeps track of the smallest index in each iteration
+            // temp is used as temporary storage
+            int minIndex, temp;
+
+            // O(n) how many times we need to go though the unsorted array
+            for (int i = 0; i < arr.Length; i++)
+            {
+                minIndex = i; // set the minIdex equal to current smallest index
+                for (int j = i; j < arr.Length; j++) // loop through each element starting at i
+                {
+                    // if the element is smaller than the current minIndex
+                    if (arr[j] < arr[minIndex])
+                    {
+                        // swap
+                        minIndex = j;
+                    }
+                }
+
+                // swap elements
+                // swap current i (which is smallest position with the smallest/min element)
+                temp = arr[i];
+                arr[i] = arr[minIndex];
+                arr[minIndex] = temp;
+            }
+        }
+    }
+}
---
+++ MergeSort Test/ProgramTests.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SortingAlgorithms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAlgorithms.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void BubbleSort_UnSortedArray_ArrayShouldToBeSorted()
+        {
+            // AAA - Arrange, Act, Assert // Arrange - setting the test up
+            int[] arr1 = { 90, 3, 2, 56, 32, 34, 65, 68, 76, 1, 0, 100, 8 };
+            // Act - This is where you call the code you are wanting to test
+            Program.BubbleSort(arr1);
+
+            // Assert - is the outcome what you expected
+            Assert.AreEqual(0, arr1[0]);
+            Assert.AreEqual(100, arr1[arr1.Length - 1]);
+            Assert.AreEqual(56, arr1[7]);
+        }
+
+        [TestMethod()]
+        public void BubbleSort_EmptyArray_ShouldReturnEmptyArray()
+        {
+            // AAA - Arrange, Act, Assert // Arrange - setting the test up
+            int[] arr1 = [];
+            // Act - This is where you call the code you are wanting to test
+            Program.BubbleSort(arr1);
+
+            // Assert - is the outcome what you expected
+            Assert.AreEqual(arr1, []);
+        }
+
+        [TestMethod()]
+        public void BubbleSort_SortedArray_ArrayShouldToBeSorted()
+        {
+            // AAA - Arrange, Act, Assert // Arrange - setting the test up
+            int[] arr1 = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            // Act - This is where you call the code you are wanting to test
+            int Iteration = Program.BubbleSort(arr1);
+
+            // Assert - is the outcome what you expected
+            Assert.AreEqual(arr1[0], 0);
+            Assert.AreEqual(arr1[arr1.Length - 1], 10);
+            Assert.AreEqual(arr1[7], 7);
+            Assert.AreEqual(Iteration, 1);
+        }
+
+        [TestMethod()]
+        public void SelectionSort_UnSortedArray_ArrayShouldBeSortedAsc()
+        {
+            int[] arrToTest = { 434, 2, 121, 43, 32, 1, 0, 78, 65, 56, 10, 8, 500, 6 };
+            Program.SelectionSort(arrToTest);
+            Assert.AreEqual(0, arrToTest[0]);
+            Assert.AreEqual(500, arrToTest[arrToTest.Length - 1]);
+            Assert.AreEqual(10, arrToTest[5]);
+        }
+
+        [TestMethod()]
+        public void SelectionSort_EmptyArray_ShouldReturnEmptyArray()
+        {
+            // AAA - Arrange, Act, Assert // Arrange - setting the test up
+            int[] arr1 = [];
+            // Act - This is where you call the code you are wanting to test
+            Program.SelectionSort(arr1);
+
+            // Assert - is the outcome what you expected
+            Assert.AreEqual([], arr1);
+        }
+    }
+}
